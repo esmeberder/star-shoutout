33a9cf361818f512b1bec0d4be1c0b199b655867
Esme Berder <esmeyangli@gmail.com>
2019-07-30 14:03:27 +0800

    authorization complete with routes and services controller

--- app/policies/service_policy.rb
+++ app/policies/service_policy.rb
@@ -14,12 +14,18 @@ class ServicePolicy < ApplicationPolicy
   end
 
   def update?
-    record.user == user
+    user_is_owner_or_admin?
     # - record: the restaurant passed to the `authorize` method in controller
     # - user:   the `current_user` signed in with Devise.
   end
 
   def destroy?
-    record.user == user
+    user_is_owner_or_admin?
+  end
+
+  private
+
+  def user_is_owner_or_admin?
+    record.user == user || user.admin
   end
 end
