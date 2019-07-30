fdde937dfe0293567a2caea6cd226878620579a9
Esme Berder <esmeyangli@gmail.com>
2019-07-30 16:13:20 +0800

    pundit booking

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -2,7 +2,36 @@ class BookingsController < ApplicationController
   before_action :set_booking, only: [:show, :edit, :update, :destroy]
 
   def index
-    # @bookings = polic
+    @bookings = policy_scope(Booking)
+  end
+
+  def new
+    @booking = Booking.new
+    authorize @booking
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    @booking.user = current_user
+    authorize @booking
+    if @booking.save
+      redirect_to bookings_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    authorize @booking
+  end
+
+  def update
+    authorize @booking
+    if @booking.update(booking_params)
+      redirect_to bookings_path
+    else
+      render :edit
+    end
   end
 
   private
@@ -11,7 +40,7 @@ class BookingsController < ApplicationController
     @booking = Booking.find(params[:id])
   end
 
-  def service_params
-    params.require(:service).permit(:accepted, :user_id, :service_id)
+  def booking_params
+    params.require(:booking).permit(:accepted, :user_id, :service_id)
   end
 end
--- app/policies/booking_policy.rb
+++ app/policies/booking_policy.rb
@@ -4,4 +4,22 @@ class BookingPolicy < ApplicationPolicy
       scope.all
     end
   end
+
+  def show?
+    user_is_owner_or_admin?
+  end
+
+  def create?
+    return true
+  end
+
+  def update?
+    record.service_id.user_id == user.id
+  end
+
+  private
+
+  def user_is_owner_or_admin?
+    record.user == user || user.admin
+  end
 end
