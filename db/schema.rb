5ed7277250f593040e63707d717c30e832562434
Esme Berder <esmeyangli@gmail.com>
2019-07-30 14:20:04 +0800

    added accepted boolean to bookings

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_30_041232) do
+ActiveRecord::Schema.define(version: 2019_07_30_061910) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2019_07_30_041232) do
   create_table "bookings", force: :cascade do |t|
     t.bigint "user_id"
     t.bigint "service_id"
+    t.boolean "accepted"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["service_id"], name: "index_bookings_on_service_id"
@@ -43,6 +44,7 @@ ActiveRecord::Schema.define(version: 2019_07_30_041232) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "star"
+    t.boolean "admin"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
