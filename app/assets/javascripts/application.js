bc8be1414fe7adb47e9fadf9f3134b94d7bd4d4f
Jinhong Wang <1320358947@qq.com>
2019-08-02 16:48:49 +0800

    play button

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,2 +1,12 @@
 //= require rails-ujs
 //= require_tree .
+filters = document.querySelectorAll("#filter")
+
+filters.forEach((filter)=>{
+  filter.addEventListener("click", (event)=>{
+    current = document.querySelector(".active")
+    current.className=current.className.replace("active","")
+    console.log(current)
+    filter.classList.add("active")
+  })
+})
